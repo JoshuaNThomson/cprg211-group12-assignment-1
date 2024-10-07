478a88ea49b6022e3489d0a188d10d5976ec6a2a
JoshuaNThomson <joshua.thomson@edu.sait.ca>
2024-10-07 14:45:26 -0600

    Update Dishwasher.cs

--- Dishwasher.cs
+++ Dishwasher.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Assignment1
 {
-    internal class Dishwasher
+    public class Vacuum
     {
+        public int ItemNumber { get; set;}
+        public string Brand { get; set; }
+        public int Quantity { get; set; }
+        public int Wattage { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string Grade { get; set; }
+        public int BatteryVoltage { get; set; }
+
+        public Vacuum(int itemNumber, string brand, int quantity, int wattage, string color, decimal price, string grade, int batteryVoltage) 
+        {
+            ItemNumber = itemNumber;
+            Brand = brand;
+            Quantity = quantity;
+            Wattage = wattage;
+            Color = color;
+            Price = price;
+            Grade = grade;
+            BatteryVoltage = batteryVoltage;
+        }
+
+        public override string ToString()
+        {
+            return $"Vacuum Details:\nItem Number: {ItemNumber}\nBrand: {Brand}\nQuantity: {Quantity}\nWattage: {Wattage}W\nColor: {Color}\nPrice: ${Price}\nGrade: {Grade}\nBattery Voltage: {BatteryVoltage}V";
+        }
     }
 }
