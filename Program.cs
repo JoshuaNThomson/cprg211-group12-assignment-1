3b87879e1e5b107f0fb6988643404033972cade6
JanKowal05 <jan.kowal@edu.sait.ca>
2024-10-09 12:47:44 -0600

    Update

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Assignment1
 {
@@ -10,6 +12,25 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
+            string path = "res/appliances.txt";
+            string[] resource = File.ReadAllLines(path);
+
+            foreach (string line in resource)
+            {
+                string[] data = line.Split(';');
+                
+                int firstNum = int.Parse("0" + data[0][0]);
+
+                Console.WriteLine(firstNum);
+
+
+
+
+
+
+            }
+
+            Console.ReadKey(); 
 
         }
     }
